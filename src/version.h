99efaa8fd355e17a68611ccd7aad1fe5927c96ba
drfrag <drfrag666@hotmail.com>
2025-10-25 12:57:35 +0200

    Revert "- Fix CI."
    This reverts commit 1a1202b96e68946afc8144f8eafb5d6734c92c29.
    
    - Change freedesktop and flatpak app name.

--- src/version.h
+++ src/version.h
@@ -98,7 +98,7 @@ const char *GetVersionString();
 #define GAMENAME "LZDoom"
 #define WGAMENAME L"LZDoom"
 #define GAMENAMELOWERCASE "lzdoom"
-#define APPID "org.zdoom.GZDoom"
+#define APPID "org.zdoom.LZDoom"
 #define FORUM_URL "http://forum.zdoom.org/"
 #define BUGS_FORUM_URL	"http://forum.zdoom.org/viewforum.php?f=2"
 
