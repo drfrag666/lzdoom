a854aa0016cca0e20cf12e73eb9fcec1e8f78595
Dileep V. Reddy <dileepvr@gmail.com>
2026-09-01 20:16:10 -0600

    Account for closed frontsectors.

--- src/rendering/hwrenderer/scene/hw_portal.cpp
+++ src/rendering/hwrenderer/scene/hw_portal.cpp
@@ -960,6 +960,7 @@ void HWPlaneMirrorPortal::DrawPortalStencil(FRenderState &state, int pass)
 		for (unsigned int i = 0; i < lines.Size(); i++)
 		{
 			bool edgeline = (i == 0) || !lines[i].seg->backsector || lines[i].seg->backsector->isClosed()
+				|| lines[i].seg->frontsector->isClosed()
 				|| (lines[i].seg->frontsector->reflect[isceiling ? sector_t::ceiling : sector_t::floor] !=
 					lines[i].seg->backsector->reflect[isceiling ? sector_t::ceiling : sector_t::floor]);
 			if (!edgeline && lines[i].seg->backsector)
